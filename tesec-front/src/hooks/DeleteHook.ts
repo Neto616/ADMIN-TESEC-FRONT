a22865577c5945ab9e8011384ec557b173ee36d7
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2026-04-21 22:44:11 -0600

    New package

--- tesec-front/src/hooks/DeleteHook.ts
+++ tesec-front/src/hooks/DeleteHook.ts
@@ -1,28 +1,34 @@
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal);
 
-export const useDelete = async (id: number, refreshData: () => Promise<void>, service: (id: number) => Promise<any>) => {
-        await Swal.fire({
-             title: '¿Eliminar producto?',
-             text: "Esta acción no se puede deshacer.",
-             icon: 'warning',
-             showCancelButton: true,
-             confirmButtonColor: '#000000',
-             cancelButtonColor: '#d33',
-             confirmButtonText: 'Sí, eliminar',
-             cancelButtonText: 'Cancelar',
-             preConfirm: async() => {
-                try {
-                    await service(id);
-                } catch (error: any) {
-                    Swal.showValidationMessage(`Error: ${error.message || 'No se pudo eliminar'}`);
-                }
-             }
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                await refreshData();
-                Swal.fire('Eliminado', 'El producto ha sido eliminado.', 'success');
-            }
-        });
-    };
+export const useDelete = async (
+  id: number,
+  refreshData: () => Promise<void>,
+  service: (id: number) => Promise<any>,
+) => {
+  await Swal.fire({
+    title: "¿Eliminar producto?",
+    text: "Esta acción no se puede deshacer.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#000000",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Sí, eliminar",
+    cancelButtonText: "Cancelar",
+    preConfirm: async () => {
+      try {
+        await service(id);
+      } catch (error: any) {
+        Swal.showValidationMessage(
+          `Error: ${error.message || "No se pudo eliminar"}`,
+        );
+      }
+    },
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      await refreshData();
+      Swal.fire("Eliminado", "El producto ha sido eliminado.", "success");
+    }
+  });
+};
